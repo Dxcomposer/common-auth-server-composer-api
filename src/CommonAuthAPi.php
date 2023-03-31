cc1cd1d414b4be987ed809e3325d70615add48ea
dxcomposer <composer@ifun.com>
2023-03-31 16:36:04 +0800

    init

--- src/CommonAuthAPi.php
+++ src/CommonAuthAPi.php
@@ -1,7 +1,7 @@
 <?php
 
 
-namespace Dx\Commonauthapi;
+namespace Dxkjcomposer\Comauthapi;
 
 
 
--- src/test.php
+++ src/test.php
@@ -1,9 +1,9 @@
 <?php
 require_once 'vendor/autoload.php';
 
-$class=new \Dx\Commonauthapi\CommonAuthAPi('11658741278226587648',['127.0.0.1:9501']);
+$class=new \Dxkjcomposer\Comauthapi\CommonAuthAPi('11658741278226587648',['127.0.0.1:9501']);
 
-//var_dump($class->login('web','haspwd'));
+var_dump($class->login('web','haspwd'));
 //var_dump($class->checkToken('e804c6f75988a41d1343690c92ebaafe'));
 //var_dump($class->loginOut('e804c6f75988a41d1343690c92ebaafe'));
 //var_dump($class->userDetail(['test1']));
